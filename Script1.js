92d120a93b2eae83205a4d4e5d261f48724ed8a6
15712972 <43040971+15712972@users.noreply.github.com>
2019-01-14 17:17:36 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+//Name:Gurpreet Kaur Chahal
+//Date:14 january 2019
+//student id: 742780
+//course code: CSD_2214_6
+
+let countDown;
+let timeinterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('clock');
+const daySpan = clock.querySelector('.days');
+const hourSpan = clock.querySelector('.hours');
+const minuteSpan = clock.querySelector(.'minutes');
+const secondSpan = clock.queryselector(.'second');
+
+
+
